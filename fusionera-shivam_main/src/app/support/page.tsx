43c8f27e81cb002b06c0d0e879deabc55d57d3f1
Shivam954629 <shivampatna44@gmail.com>
2026-05-16 17:25:09 +0530

    remove nested duplicate folder

--- fusionera-shivam_main/src/app/support/page.tsx
+++
@@ -1,5 +0,0 @@
-import Support from "@/components/Support";
-import React from "react";
-export default function SupportPage() {
-  return <Support />;
-}
--- fusionera-shivam_main/src/app/terms/page.tsx
+++
@@ -1,5 +0,0 @@
-import react from "react";
-import Terms from "@/components/Terms";
-export default function TermsPage() {
-  return <Terms />;
-}
