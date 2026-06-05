4767c9533752cd4ee292c5b3a7388c132e3ef43c
Shivam954629 <shivampatna44@gmail.com>
2026-06-05 13:45:41 +0530

    Add linkdln insight tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,6 +96,28 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-P5LLJNZ9');`,
           }}
         />
+        {/* LinkedIn Insight Tag */}
+        <Script
+          id="linkedin-insight"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `_linkedin_partner_id = "9473356";
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+(function(l) {
+if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+window.lintrk.q=[]}
+var s = document.getElementsByTagName("script")[0];
+var b = document.createElement("script");
+b.type = "text/javascript";b.async = true;
+b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+s.parentNode.insertBefore(b, s);})(window.lintrk);`,
+          }}
+        />
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img height="1" width="1" style={{ display: "none" }} alt="" src="https://px.ads.linkedin.com/collect/?pid=9473356&fmt=gif" />
+        </noscript>
       </body>
     </html>
   );
